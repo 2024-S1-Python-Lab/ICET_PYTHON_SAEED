a9e81827bae2dcb49604457557ad10eec6248d1b
saeed9961 <saeedkunjumon03@gmail.com>
2025-03-19 14:24:17 +0530

    java

---
+++ PYTHON-SAEED/java/add.java
@@ -0,0 +1,11 @@
+public class add
+{
+public static void main(String[]args)
+{
+int x=10;
+int y=15;
+int sum=x+y;
+}
+System.out.println("sum =");
+}
+
---
+++ PYTHON-SAEED/java/sample.java
@@ -0,0 +1,11 @@
+public class sample
+{
+public static void main(String[]args)
+{
+int num=10;
+if(num>0);
+{
+System.out.println("the number is positive.");
+}
+}
+}
